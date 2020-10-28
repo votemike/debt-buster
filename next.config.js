97e220dfb74d06d30a6e37940e1a736e0af7b526
Michael Gwynne <michael@votemike.co.uk>
2020-10-28 00:04:46 +0000

    Fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
-const withPlugins = require('next-compose-plugins')
+const withPlugins = require('next-compose-plugins');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-})
+});
+const withPWA = require('next-pwa');
+
 module.exports = withPlugins([
   [withBundleAnalyzer({})],
-  [withPWA({
+  [withPWA, {
     pwa: {
       dest: 'public'
     }
-  })]
+  }]
 ])
