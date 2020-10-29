b83a051898a37fc6f36b542a5f0fafe9b258d969
Michael Gwynne <michael@votemike.co.uk>
2020-10-29 21:18:08 +0000

    More

---
+++ components/add-debt-form.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import styles from "../styles/AddDebtForm.module.css";
+
+class AddDebtForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = this.getInitialFormState();
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  getInitialFormState() {
+    return {name: '', amount: '', apr: ''}; // servicing, 0% balance transfer
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({[name]: value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.addDebtHandler(this.state);
+    this.setState(this.getInitialFormState());
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h2>Add a Debt</h2>
+        <form onSubmit={this.handleSubmit}>
+          <div className={styles.formRow}>
+            <label>Debt/Loan/Card Name</label>
+
+            <input name="name" value={this.state.name} onChange={this.handleInputChange}
+                   placeholder="Car Loan" required/>
+          </div>
+          <div className={styles.formRow}>
+            <label>Debt amount</label>
+            <input name="amount" value={this.state.amount} onChange={this.handleInputChange}
+                   type="number" placeholder="871" step="any" min="0" required/>
+          </div>
+          <div className={styles.formRow}>
+            <label>APR</label>
+            <input name="apr" value={this.state.apr} onChange={this.handleInputChange}
+                   type="number" placeholder="29.9" step="any" min="0" required/>
+          </div>
+          <div className={styles.formRow}>
+            <button type="submit" className="button is-primary">
+              Add Debt
+            </button>
+          </div>
+        </form>
+      </React.Fragment>
+    );
+  }
+}
+
+export default AddDebtForm
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,45 @@
 import Head from 'next/head'
+import {useState} from 'react';
 import styles from '../styles/Home.module.css'
+import AddDebtForm from "../components/add-debt-form";
 
 export default function Home() {
+  const [navIsOpen, setNavOpen] = useState(false);
+
+  function toggleNav() {
+    setNavOpen(!navIsOpen);
+  }
+
+  const [debts, setDebts] = useState([]);
+
+  const addDebtHandler = formState => {
+    const debt = {name: formState.name, amount: parseFloat(formState.amount), apr: parseFloat(formState.apr)};
+    setDebts([...debts, debt]);
+    setNavOpen(false);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Debt Buster</title>
         {/*<link rel="icon" href="/favicon.ico" />*/}
       </Head>
-      <div className={styles.quickView}>
-        {/*These things should all get smaller when you scroll*/}
-        {/*This div should be sticky to the top*/}
-        <div>Your Debts (X)</div>
-        <button>Add Debt</button>
-        <div>Total to be paid off (current + interest)</div>
-        <div>Date it will be paid off by</div>
-        <div>Total:Cards £3,500Interest £967Monthly payment £255</div>
-      </div>
+      <section className={styles.topBit}>
+        <div className={styles.quickView}>
+          {/*These things should all get smaller when you scroll*/}
+          {/*This div should be sticky to the top*/}
+          <div>
+            <div>Your Debts (£{debts.reduce((a, b) => a + b.amount, 0)})</div>
+            <div>Paid off by (Y)</div>
+          </div>
+
+          <button onClick={toggleNav}>{navIsOpen ? 'Close' : 'Add Debt'}</button>
+          {/*<div>Total to be paid off (current + interest)</div>*/}
+
+          {/*<div>Total:Cards £3,500Interest £967Monthly payment £255</div>*/}
+        </div>
+        {navIsOpen && <AddDebtForm addDebtHandler={addDebtHandler}/>}
+      </section>
       <main className={styles.main}>
         <h1 className={styles.title}>
           Debt Buster
@@ -26,48 +49,52 @@ export default function Home() {
           Use this app to enter all your current debt and work out the way to pay them off fastest.
         </p>
 
+        {debts.length > 0 && (<ul>
+          {debts.map((debt, index) => (
+            <li key={debt.name}>{debt.name} - £{debt.amount} - {debt.apr}%</li>
+          ))}
+        </ul>)}
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        <div>Loads of content</div>
+        </main>
 
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-        <div>Loads of content</div>
-      </main>
-
-      <footer className={styles.footer}>
+        <footer className={styles.footer}>
         <div>Money Advice Service:
-          <a href="https://www.moneyadviceservice.org.uk/en/tools/debt-advice-locator" target="_blank" rel="noopener noreferrer">Debt Advice Locator</a>
-          <a href="https://www.moneyadviceservice.org.uk/en/tools/budget-planner" target="_blank" rel="noopener noreferrer">Budget Planner</a>
-          <a href="https://www.moneyadviceservice.org.uk/en/categories/debt-and-borrowing" target="_blank" rel="noopener noreferrer">Debt and Borrowing</a>
-          <a href="https://www.moneyadviceservice.org.uk/en/categories/help-with-loans" target="_blank" rel="noopener noreferrer">Help with loans</a>
+        <a href="https://www.moneyadviceservice.org.uk/en/tools/debt-advice-locator" target="_blank" rel="noopener noreferrer">Debt Advice Locator</a>
+        <a href="https://www.moneyadviceservice.org.uk/en/tools/budget-planner" target="_blank" rel="noopener noreferrer">Budget Planner</a>
+        <a href="https://www.moneyadviceservice.org.uk/en/categories/debt-and-borrowing" target="_blank" rel="noopener noreferrer">Debt and Borrowing</a>
+        <a href="https://www.moneyadviceservice.org.uk/en/categories/help-with-loans" target="_blank" rel="noopener noreferrer">Help with loans</a>
         </div>
         <div>Money Saving Expert:
-          <a href="https://www.moneysavingexpert.com/loans/debt-help-plan/" target="_blank" rel="noopener noreferrer">Debt Help</a>
-          <a href="https://www.moneysavingexpert.com/eligibility/credit-cards/search/?goal=CC_BALTRANSFERV2" target="_blank" rel="noopener noreferrer">Credit Card Eligibilty Calculator</a>
+        <a href="https://www.moneysavingexpert.com/loans/debt-help-plan/" target="_blank" rel="noopener noreferrer">Debt Help</a>
+        <a href="https://www.moneysavingexpert.com/eligibility/credit-cards/search/?goal=CC_BALTRANSFERV2" target="_blank" rel="noopener noreferrer">Credit Card Eligibilty Calculator</a>
         </div>
-      </footer>
-    </div>
-  )
-}
+        </footer>
+        </div>
+        )
+        }
